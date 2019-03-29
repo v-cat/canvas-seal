99a205b8f72eeca7f9f1da3155e0353eb35bcedd
杨冬林 <1936881152@qq.com>
2019-03-29 17:23:29 +0800

    暂停版本

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = new express();
-
-app.use(express.static(__dirname + '/'));
-
-app.listen(3000, () => {
-    console.log('server start')
-})
--- 未命名文件夹/README.md
+++
@@ -1,5 +0,0 @@
-# canvas-seal
-A demo  auto generates seals  with canvas.
-
-It provides configurable params to draw your own seals.
-
